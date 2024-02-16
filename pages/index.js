059189f4d78aed0ac9ea3d011bf698067056d985
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-02-16 20:03:23 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,23 @@ import Head from "next/head";
 import Layout from "./layout";
 import Home from "./Home";
 import { useRouter } from "next/router";
+import { initializeApp } from "firebase/app";
 import TableData from "../components/TableData";
 import NameSelected from "../components/NameSelected";
 
 const HomePage = () => {
-  const router = useRouter();
+  const firebaseConfig = {
+    apiKey: "AIzaSyDaN0lsrRosXmn9Ho5DvLTYUQ80aVG3Xck",
+    authDomain: "baby-name-734fd.firebaseapp.com",
+    projectId: "baby-name-734fd",
+    storageBucket: "baby-name-734fd.appspot.com",
+    messagingSenderId: "533945436532",
+    appId: "1:533945436532:web:212951d50f7b686d334421",
+  };
 
-  // Check if the current page is the home page
+  const app = initializeApp(firebaseConfig);
+
+  const router = useRouter();
   const isHomePage = router.pathname === "/";
 
   return (
@@ -26,7 +36,6 @@ const HomePage = () => {
         <Home />
       ) : (
         <Layout>
-          {/* Render other pages here based on the router pathname */}
           {router.pathname.startsWith("/india") && <TableData />}
           {router.pathname === "/india/name-selected" && <NameSelected />}
         </Layout>
