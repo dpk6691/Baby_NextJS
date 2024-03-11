8a372ee409ebf639d762be22889475f38d230420
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-03-11 09:44:54 +0530

    update

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import zlib from "zlib";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
-//test
 
 export default async function handler(req, res) {
   try {
@@ -13,9 +12,22 @@ export default async function handler(req, res) {
       .select(
         "gender, culture, name, language, meaning_of_name, meaning_in_language"
       );
+
     if (error) throw error;
 
-    res.status(200).json(data);
+    // Convert data to JSON string
+    const jsonData = JSON.stringify(data);
+
+    // Compress data using gzip
+    const compressedData = zlib.gzipSync(jsonData);
+
+    // Set response headers to indicate gzip compression
+    res.setHeader("Content-Encoding", "gzip");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Length", compressedData.length);
+
+    // Send compressed data as response
+    res.status(200).end(compressedData);
   } catch (error) {
     console.error("Error fetching data:", error.message);
     res.status(500).json({ error: "Failed to fetch data" });
