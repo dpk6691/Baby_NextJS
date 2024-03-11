0a91dc0cea3986ef76f3a52a78dba8e5f4642af6
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-03-11 08:49:06 +0530

    Add files via upload

---
+++ pages/api/getData.js
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_ANON_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default async function handler(req, res) {
+  try {
+    const { data, error } = await supabase
+      .from("India")
+      .select(
+        "gender, culture, name, language, meaning_of_name, meaning_in_language"
+      );
+    if (error) throw error;
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+}
