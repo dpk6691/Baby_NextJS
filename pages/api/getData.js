4629b2a6d482faccb6bd2c1a287d9f31f6de5ea4
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-06-12 09:56:12 +0530

    update

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 import zlib from "zlib";
 
-const supabaseUrl = https://qycwdzeatinkjfsvvmnj.supabase.co;
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Y3dkemVhdGlua2pmc3Z2bW5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczOTYyNDIsImV4cCI6MjAyMjk3MjI0Mn0.CYLbLyI70TCseUmfFTQ1E9J-A5zE9ad8W32Uw_ttOIs;
+const supabaseUrl = qycwdzeatinkjfsvvmnj.supabase.co;
+const supabaseKey =
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+    .eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Y3dkemVhdGlua2pmc3Z2bW5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczOTYyNDIsImV4cCI6MjAyMjk3MjI0Mn0
+    .CYLbLyI70TCseUmfFTQ1E9J - A5zE9ad8W32Uw_ttOIs;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase URL or Key");
