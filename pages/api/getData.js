b9399dbdc04c0dc5688c9faa773a59b5e226e40f
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-06-24 11:41:07 +0530

    update

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -3,7 +3,12 @@ import zlib from "zlib";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  fetch: (url, options) => {
+    options.timeout = 30000; // Set timeout to 30 seconds
+    return fetch(url, options);
+  },
+});
 
 export default async function handler(req, res) {
   try {
@@ -14,8 +19,8 @@ export default async function handler(req, res) {
       );
 
     if (error) throw error;
-    const jsonData = JSON.stringify(data);
 
+    const jsonData = JSON.stringify(data);
     const compressedData = zlib.gzipSync(jsonData);
 
     res.setHeader("Content-Encoding", "gzip");
@@ -24,7 +29,7 @@ export default async function handler(req, res) {
 
     res.status(200).end(compressedData);
   } catch (error) {
-    console.error("Error fetching data:", error.message);
+    console.error("Error fetching data:", error);
     res.status(500).json({ error: "Failed to fetch data" });
   }
 }
